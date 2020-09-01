9139db29d15f1982d4291f17e7b67e61f7e388a6
Reine Alice <reinelic@yahoo.fr>
2020-09-01 11:10:47 +0200

    setup of models

--- server.js
+++ server.js
@@ -3,14 +3,31 @@ const chalk = require("chalk");
 const morgan = require("morgan");
 const debug = require("debug")("app");
 
+const connectDB = require('./config/db');
+
 const app = express();
+
+
+
+
+//DATABASE
+
+connectDB();
 
 //MIDDLEWARE 
 
 app.use(morgan('tiny'));
 
+
+
+
+//ROUTES
+
+app.get("/", (req, res) => res.send(" Hello Parents"));
+
+
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => debug(`App is listening to  ${chalk.red(PORT)}`));
 
-app.get("/", (req, res) => res.send(" Hello Parents"));
