eed7a01591ff9813c1e2a63d57bf6028ad07a066
Reine Alice <reinelic@yahoo.fr>
2020-09-01 11:25:18 +0200

    committed  creation of models

---
+++ models/Place.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+
+const placeSchema = new mongoose.Schema({
+
+    name: {
+        type: String
+    },
+    category:{
+        type:string
+    },
+    badge: {
+        type: String
+    },
+    address: {
+        type: String
+    },
+    photo: {
+
+        type: String,
+    },
+    contact:{
+        type:Number
+    },
+
+    reviews:[{
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'user'
+        }
+    }]
+})
+
+const Place = mongoose.model('place', PlaceSchema);
+
+module.exports = Place;
